1b9a8015fe9f5045936031d866b1f4f832033d5f
danilqweasd12321 <37021861+danilqweasd12321@users.noreply.github.com>
2018-11-04 21:13:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var WebSocket = require('ws');
 var wss = new WebSocket.Server({
 	port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080
 });
+var wss1 = new WebSocket.Server({
+	port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080
+});
 
 wss.on('connection', function (cws, req){
 	console.log(`New connection from ${req.connection.remoteAddress}`);
@@ -9,10 +12,20 @@ wss.on('connection', function (cws, req){
 		origin:"http://www.multiplayerpiano.com"
 	});
 
+wss1.on('connection', function (cws1, req1){
+	console.log(`New connection from ${req1.connection.remoteAddress}`);
+	var tws1 = new WebSocket("ws://23.95.115.204:8080", {
+		origin:"http://mpp.terrium.net"
+	});
+
 	// client to server
 	var messageBuffer = [];
 	tws.on('open', function(){
 		for (let message of messageBuffer) tws.send(message);
+		messageBuffer = undefined;
+	});
+	tws1.on('open', function(){
+		for (let message of messageBuffer) tws1.send(message);
 		messageBuffer = undefined;
 	});
 	cws.on('message', function(message){
@@ -22,13 +35,25 @@ wss.on('connection', function (cws, req){
 		if (tws.readyState == WebSocket.OPEN) tws.send(message);
 		else messageBuffer.push(message);
 	});
+	cws1.on('message', function(message){
+		try{
+		//wssender.send('geted message, proxy ' + process.env.numberproxy + ': ' + message)
+		} catch(e) {}
+		if (tws1.readyState == WebSocket.OPEN) tws1.send(message);
+		else messageBuffer.push(message);
+	});
 	cws.on('close', function(){
 		tws.close();
 		console.log(`Closing connection from ${req.connection.remoteAddress}`)
 		messageBuffer = undefined;
 	});
+	cws1.on('close', function(){
+		tws1.close();
+		console.log(`Closing connection from ${req1.connection.remoteAddress}`)
+		messageBuffer = undefined;
+	});
 	cws.on('error', console.error);
-
+	cws1.on('error', console.error);
 	// server to client
 	tws.on('message', function(message){
 	try{
@@ -40,4 +65,14 @@ wss.on('connection', function (cws, req){
 		cws.close();
 	});
 	tws.on('error', console.error);
+	tws1.on('message', function(message){
+	try{
+		//wssender.send('geted message, proxy ' + process.env.numberproxy + ': ' + message)
+	} catch(e) {}
+		if (cws1.readyState == WebSocket.OPEN) cws1.send(message);
+	});
+	tws1.on('close', function(){
+		cws1.close();
+	});
+	tws1.on('error', console.error);
 });

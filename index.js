ebd8dfde2cbdc9f2da199e8f141e50cb8f124a45
danilqweasd12321 <37021861+danilqweasd12321@users.noreply.github.com>
2018-11-04 21:18:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,21 +11,9 @@ wss.on('connection', function (cws, req){
 	var tws = new WebSocket("ws://www.multiplayerpiano.com:443", {
 		origin:"http://www.multiplayerpiano.com"
 	});
-
-wss1.on('connection', function (cws1, req1){
-	console.log(`New connection from ${req1.connection.remoteAddress}`);
-	var tws1 = new WebSocket("ws://23.95.115.204:8080", {
-		origin:"http://mpp.terrium.net"
-	});
-
-	// client to server
 	var messageBuffer = [];
 	tws.on('open', function(){
 		for (let message of messageBuffer) tws.send(message);
-		messageBuffer = undefined;
-	});
-	tws1.on('open', function(){
-		for (let message of messageBuffer) tws1.send(message);
 		messageBuffer = undefined;
 	});
 	cws.on('message', function(message){
@@ -35,26 +23,12 @@ wss1.on('connection', function (cws1, req1){
 		if (tws.readyState == WebSocket.OPEN) tws.send(message);
 		else messageBuffer.push(message);
 	});
-	cws1.on('message', function(message){
-		try{
-		//wssender.send('geted message, proxy ' + process.env.numberproxy + ': ' + message)
-		} catch(e) {}
-		if (tws1.readyState == WebSocket.OPEN) tws1.send(message);
-		else messageBuffer.push(message);
-	});
 	cws.on('close', function(){
 		tws.close();
 		console.log(`Closing connection from ${req.connection.remoteAddress}`)
 		messageBuffer = undefined;
 	});
-	cws1.on('close', function(){
-		tws1.close();
-		console.log(`Closing connection from ${req1.connection.remoteAddress}`)
-		messageBuffer = undefined;
-	});
 	cws.on('error', console.error);
-	cws1.on('error', console.error);
-	// server to client
 	tws.on('message', function(message){
 	try{
 		//wssender.send('geted message, proxy ' + process.env.numberproxy + ': ' + message)
@@ -65,6 +39,33 @@ wss1.on('connection', function (cws1, req1){
 		cws.close();
 	});
 	tws.on('error', console.error);
+	});
+wss1.on('connection', function (cws1, req1){
+	console.log(`New connection from ${req1.connection.remoteAddress}`);
+	var tws1 = new WebSocket("ws://23.95.115.204:8080", {
+		origin:"http://mpp.terrium.net"
+	});
+
+	// client to server
+	var messageBuffer = [];
+	tws1.on('open', function(){
+		for (let message of messageBuffer) tws1.send(message);
+		messageBuffer = undefined;
+	});
+	cws1.on('message', function(message){
+		try{
+		//wssender.send('geted message, proxy ' + process.env.numberproxy + ': ' + message)
+		} catch(e) {}
+		if (tws1.readyState == WebSocket.OPEN) tws1.send(message);
+		else messageBuffer.push(message);
+	});
+	cws1.on('close', function(){
+		tws1.close();
+		console.log(`Closing connection from ${req1.connection.remoteAddress}`)
+		messageBuffer = undefined;
+	});
+	cws1.on('error', console.error);
+	// server to client
 	tws1.on('message', function(message){
 	try{
 		//wssender.send('geted message, proxy ' + process.env.numberproxy + ': ' + message)
